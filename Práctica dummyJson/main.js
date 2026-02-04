8ee356cc91ff761139ce6ae29cc4ef17bf955ef5
JuanOlvera06 <20240982@uthh.edu.mx>
2026-02-04 12:40:35 -0600

    Subida

--- Práctica dummyJson/main.js
+++ Práctica dummyJson/main.js
@@ -1,5 +1,8 @@
 const urlApi = "https://dummyjson.com/products";
 
+const params = new URLSearchParams(window.location.search);
+const idProducto = params.get("id");
+
 const cargarProductos = () => {
   // Usamos fetch para hacer la petición HTTP
   fetch(urlApi)
@@ -20,22 +23,26 @@ const cargarProductos = () => {
     });
 };
 
-
 // Función encargada de manipular el DOM
 const mostrar = (productos) => {
   const contenedorProductos = document.getElementById("contenedor-productos");
+
   contenedorProductos.innerHTML = "";
+  
 
   // Recorremos cada producto
   productos.forEach((producto) => {
     const tarjeta = document.createElement("article");
 
-    const clases = "bg-white rounded-lg shadow-md p-5 hover:shadow-xl transition";
+    const clases =
+      "bg-white rounded-lg shadow-md p-5 hover:shadow-xl transition";
     tarjeta.classList.add(...clases.split(" "));
 
     tarjeta.innerHTML = `
+    <a href="vistaDetalle.html?id=${producto.id}">
       <img src="${producto.images[0]}" alt="${producto.title}"
            style="object-fit:contain;height:300px;width:100%;">
+             </a>
       <h3>${producto.title}</h3>
       <p><strong>Categoría:</strong> ${producto.category}</p>
       <p><strong>Precio:</strong> $${producto.price}</p>
@@ -46,4 +53,89 @@ const mostrar = (productos) => {
 };
 
 
+//Funcion al hacer una busqueda una busqueda, para saber que hacer si funciona o de lo contrario falla
+const accionBuscar = () => {
+  const filtro = document.getElementById("simple-search").value.trim();
 
+  if (filtro === "") {
+    cargarProductos(); // si no hay texto, muestra todo
+  } else {
+    buscarProductos(filtro); // si hay texto, busca
+  }
+};
+
+
+//Funcion que trae los productos pero filtrandolos por la busqueda
+const buscarProductos = (texto) => {
+  fetch(urlApi+"/"+"search?q="+texto)
+    .then(respuesta => respuesta.json())
+    .then(data => {
+      mostrar(data.products);
+    })
+    .catch(error => {
+      console.error("Error en la búsqueda:", error);
+    });
+};
+
+
+
+
+
+const cargarProducto = () => {
+  // Usamos fetch para hacer la petición HTTP
+  fetch(urlApi + "/" + idProducto)
+    .then((respuesta) => respuesta.json()) // Convertimos la respuesta cruda a formato JSON
+    .then((data) => {
+      // La API devuelve un objeto con una propiedad 'items' que contiene el array
+      const producto = data;
+
+      console.log("Datos recibidos:", producto); // Debugging en consola
+
+      // Llamamos a la función que se encarga de dibujar en pantalla
+      mostrarProducto(producto);
+    })
+    .catch((error) => {
+      // Buena práctica: Manejar errores por si falla la red o la API
+      console.error("Error al cargar los productos:", error);
+      alert("Hubo un error al cargar los datos. Revisa la consola.");
+    });
+};
+
+const mostrarProducto = (producto) => {
+  const contenedorProducto = document.getElementById("contenedor-producto");
+    const contenedorReviews = document.getElementById("contenedor-reviews");
+  contenedorProducto.innerHTML = "";
+  contenedorReviews.innerHTML = "";   // Limpiar reviews
+
+  // Recorremos cada producto
+
+  const tarjeta = document.createElement("article");
+  
+
+  const clases =
+    "bg-white rounded-lg shadow-md p-5 hover:shadow-xl transition w-[85%] md:w-[50%] max-w-4xl mx-auto";
+  tarjeta.classList.add(...clases.split(" "));
+
+  tarjeta.innerHTML = `
+
+      <img src="${producto.images[0]}" alt="${producto.title}"
+           style="object-fit:contain;height:300px;width:100%;">
+      <h3>${producto.title}</h3>
+      <p><strong>Categoría:</strong> ${producto.category}</p>
+      <p><strong>Precio:</strong> $${producto.price}</p>
+    `;
+
+    
+  producto.reviews.forEach((review) => {
+      contenedorReviews.innerHTML +=
+        '<div class="post">' +
+          '<div class="post-title"> <strong>Comenatrio: </strong> ' +
+          review.comment +
+          '</div>' +
+          '<p><strong>ReviewName: </strong>'+ review.reviewerName+'</p>'+
+          '<p><strong>Rating: </strong>'+ review.rating+'</p>'
+        '</div>';
+    });
+
+  contenedorProducto.appendChild(tarjeta);
+};
